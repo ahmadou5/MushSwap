943072571b6f9f31124d40d6fc7c2b6805fb92fc
ahmadou5 <ahmadlasauwal@gmail.com>
2023-12-25 05:51:48 +0300

    'new40'

--- packages/tokens/src/helpers.ts
+++ packages/tokens/src/helpers.ts
@@ -9,6 +9,7 @@ export const EMPTY_LIST: TokenAddressMap<ChainId> = {
   [ChainId.GOERLI]: {},
   [ChainId.BSC]: {},
   [ChainId.BSC_TESTNET]: {},
+  [ChainId.FON_CHAIN]: {},
 }
 
 export function serializeTokens(unserializedTokens) {
